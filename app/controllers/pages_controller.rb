7aaf08e5255d58000e889e4ee955ff322be33622
Johan Codinha <johan@codinha.net>
2016-05-12 09:01:16 +1000

    working in progress for display page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,24 @@ class PagesController < ApplicationController
 
   def display
   	@stops = Trip.find_by(disp_url: params[:disp_url]).stops.order(:arrived_at)
+ 		@geo_json = [{
+	    "type" => "FeatureCollection",
+	    "features" => [{
+	        "type" => "Feature",
+	        "geometry" => {
+	          "type" => "Point",
+	          "coordinates" => [ 35.90556, 14.47611 ]},
+	        "properties" => {
+	          "marker-symbol" => "airport",
+	          "marker-size" => "medium",
+	          "marker-color" => "#FF0000",
+	          "title" => "Saint John"
+	        }
+	    }]
+	  	}].to_json
   end
 end
+
+
+
+
