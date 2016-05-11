0ac4c2877d6f562ba07e13a47e45edffc3cfbe12
Karina Thompson <klthompsn8@gmail.com>
2016-05-11 10:57:57 +1000

    Moved js into separate file and refactored

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,165 @@
+
+var mapAccessKey = "<%= ENV['MAP_ACCESS_KEY'] %>";
+
+// Globals
+var tripifyMap;
+var lastChosenPlace = {
+  lat: null,
+  long: null,
+  placeName: null
+};
+var myPlaces =[];
+
+// Map display & turn autocomplete on
+$(document).ready(function() {
+  L.mapbox.accessToken = mapAccessKey;
+  tripifyMap = L.mapbox.map('map', 'mapbox.streets').setView([0, 0], 2);
+  $("#placefinder").easyAutocomplete(options);  
+  $('#addplace').on('click', addPlace);
+});
+
+//date picker widget
+$(function() {
+  $("#datepicker").datepicker({
+  dateFormat: "dd-mm-yy",
+  changeMonth: true,
+  changeYear: true
+  })
+});
+
+var addPin = function(map) {
+  var geojson = [
+  {
+    "type": "FeatureCollection",
+    "features": [ {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ lastChosenPlace.lat, lastChosenPlace.long ]
+        },
+        "properties": {
+          "marker-symbol": "airport",
+          "marker-size": "medium",
+          "marker-color": "#FF0000",
+          "title": lastChosenPlace.placeName
+        }
+    }]
+  }];
+
+  var pinLayer = L.mapbox.featureLayer().addTo(map);
+  pinLayer.setGeoJSON(geojson);  
+};
+
+//date picker widget
+$(function() {
+  $("#datepicker").datepicker({
+  dateFormat: "dd-mm-yy",
+  changeMonth: true,
+  changeYear: true
+  })
+});
+
+//autocomplete place names
+var options = {
+  url: function(input) { 
+    return "https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + 
+    ".json?types=place%2Ccountry%2Cregion&access_token=" + mapAccessKey;
+  },
+
+  getValue: "place_name",
+
+  ajaxSettings: {
+      dataType: "json"
+  },
+
+  listLocation: "features",
+
+  requestDelay: 300,
+
+  theme: "round",
+
+  placeholder: "Enter a place",
+
+  list: {
+    onChooseEvent: function() {
+      var coords = $('#placefinder').getSelectedItemData().center;
+      var placeName = $('#placefinder').getSelectedItemData().place_name;
+
+      lastChosenPlace.lat = coords[0];
+      lastChosenPlace.long = coords[1];
+      lastChosenPlace.placeName = placeName;
+    }
+  }
+};
+
+
+
+var addPlace = function(event) {
+  var $newPlace = $('<p>').text($('#placefinder').val());
+  var $newDate = $('<span>').text(' ' + $('#datepicker').val());
+  $newPlace.append($newDate);
+  $('#tripform').append($newPlace);
+
+  var obj = {};
+  obj['date'] = $('#datepicker').val();
+  obj['country']= $('#placefinder').val();
+  myPlaces.push(obj);
+  makePieChart();  
+  addPin(tripifyMap);
+};
+
+
+var makePieChart = function() {
+  // clears chart every time a new city is added
+  $('#chart').empty();
+  var newarray =[];
+  for(var i=0;i<myPlaces.length;i++) {
+    var oneDay = 24*60*60*1000;
+    // creates date object
+    firstDate = myPlaces[i].date.split("-").reverse().join("-");
+    firstDateObj = new Date(firstDate);
+    var object = {};
+    if (myPlaces[i+1] == undefined) {
+      var today = new Date();
+      var secondDate = today.getDate() + (today.getMonth()+1) + today.getFullYear();
+      var secondDateObj = new Date(secondDate);
+      var diffDays = Math.round(Math.abs((secondDateObj.getTime() - firstDateObj.getTime())/(oneDay)));
+      object['country'] = myPlaces[i].country;
+      object['count'] = diffDays;
+      newarray.push(object);
+    }
+    else {
+      var secondDate = myPlaces[i+1].date.split("-").reverse().join("-");
+      var secondDateObj = new Date(secondDate);
+      // calculate no. of days
+      var diffDays = Math.round(Math.abs((secondDateObj.getTime() - firstDateObj.getTime())/(oneDay)));
+      object['country'] = myPlaces[i].country;
+      object['count'] = diffDays;
+      newarray.push(object);
+    }
+    console.log(newarray);
+  }
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;
+    var color = d3.scale.ordinal().range(['#A60F2B', '#648C85', '#B3F2C9', '#528C18', '#C3F25C']);
+    var svg = d3.select('#chart')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .append('g')
+                .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+    var arc = d3.svg.arc().outerRadius(radius);
+    var pie = d3.layout.pie().value(function(d) { return d.count; });
+    var path = svg.selectAll('path')
+                  .data(pie(newarray))
+                  .enter()
+                  .append('path')
+                  .attr('d', arc)
+                  .attr('fill', function(d, i) {
+                    return color(d.data.country);
+                  });
+
+}
+
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.easy-autocomplete.min.js
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
